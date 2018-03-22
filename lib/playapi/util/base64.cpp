ac5392d4245cf3751db5179c122ce0691d91e9ee
MCMrARM <mcmrarm@gmail.com>
2018-03-22 21:01:40 +0100

    Base64 lib improvements

--- lib/playapi/util/base64.cpp
+++ lib/playapi/util/base64.cpp
@@ -4,20 +4,18 @@
 
 using namespace playapi;
 
-char base64::table[65] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-unsigned char base64::reverse_table[256];
-bool base64::reverse_table_initialized = false;
+base64::character_table base64::standard ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/");
+base64::character_table base64::url_safe ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_");
 
-void base64::init_reverse_table() {
+base64::character_table::character_table(const char table[65]) : table(table) {
     memset(reverse_table, 255, sizeof(reverse_table));
     for (int i = 0; i < 64; i++) {
         reverse_table[table[i]] = (unsigned char) i;
     }
     reverse_table['='] = 64;
-    reverse_table_initialized = true;
 }
 
-std::string base64::encode(const std::string& input) {
+std::string base64::encode(const std::string& input, character_table& table) {
     size_t block_count = (input.size() + 2) / 3;
     std::string outp;
     outp.resize(block_count * 4);
@@ -30,17 +28,16 @@ std::string base64::encode(const std::string& input) {
         n1 = (unsigned char) (((i0 & 3) << 4) | ((i1 >> 4) & 0xf));
         n2 = (unsigned char) (((i1 & 0xf) << 2) | ((i2 >> 6) & 3));
         n3 = (unsigned char) (i2 & 0x3f);
-        outp[i * 4] = table[n0];
-        outp[i * 4 + 1] = table[n1];
-        outp[i * 4 + 2] = input.length() > i * 3 + 1 ? table[n2] : '=';
-        outp[i * 4 + 3] = input.length() > i * 3 + 2 ? table[n3] : '=';
+        outp[i * 4] = table.table[n0];
+        outp[i * 4 + 1] = table.table[n1];
+        outp[i * 4 + 2] = input.length() > i * 3 + 1 ? table.table[n2] : '=';
+        outp[i * 4 + 3] = input.length() > i * 3 + 2 ? table.table[n3] : '=';
     }
     return std::move(outp);
 }
 
-std::string base64::decode(const std::string& input, const char* skip_chars) {
-    if (!reverse_table_initialized)
-        init_reverse_table();
+std::string base64::decode(const std::string& input, character_table& table, bool allow_no_pad,
+                           const char* skip_chars) {
     std::string output;
     output.reserve((input.size() + 3) / 4 * 3);
     unsigned char i[4];
@@ -48,15 +45,21 @@ std::string base64::decode(const std::string& input, const char* skip_chars) {
     while (p < input.size()) {
         for (int n = 0; n < 4; n++) {
             while (true) {
-                if ((i[n] = reverse_table[input[p]]) != 255) {
+                if (p >= input.size()) {
+                    i[n] = 0;
+                    break;
+                }
+
+                if ((i[n] = table.reverse_table[input[p]]) != 255) {
                     if ((i[n] == 64 && n < 2) || (n == 3 && i[n - 1] == 64 && i[n] != 64))
                         throw std::runtime_error("Invalid '=' character");
-                    p++;
+                    if (++p == input.size() && !allow_no_pad)
+                        throw std::runtime_error("Unexpected end of input");
                     break;
                 }
                 if (strchr(skip_chars, input[p]) == NULL)
-                    throw std::runtime_error("Invalid character at " + p);
-                if (++p == input.size())
+                    throw std::runtime_error("Invalid character at " + std::to_string(p));
+                if (++p == input.size() && !allow_no_pad)
                     throw std::runtime_error("Unexpected end of input");
             }
         }
--- lib/playapi/util/base64.h
+++ lib/playapi/util/base64.h
@@ -6,19 +6,24 @@ namespace playapi {
 
 class base64 {
 
-private:
-
-    static char table[65];
-    static unsigned char reverse_table[256];
-    static bool reverse_table_initialized;
-
-    static void init_reverse_table();
-
 public:
+    class character_table {
+    private:
+        friend class base64;
+        const char* table;
+        unsigned char reverse_table[256];
 
-    static std::string encode(const std::string& input);
+    public:
+        character_table(const char table[65]);
+    };
 
-    static std::string decode(const std::string& input, const char* skip_chars = "\r\n");
+    static character_table standard;
+    static character_table url_safe;
+
+    static std::string encode(const std::string& input, character_table& table = standard);
+
+    static std::string decode(const std::string& input, character_table& table = standard,
+                              bool allow_no_pad = false, const char* skip_chars = "\r\n");
 
 };
 
